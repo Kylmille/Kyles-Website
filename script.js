746ede6885664a908e900e7d1077f9c385176280
Kyle <Kylesiphone625@gmail.com>
2023-06-16 18:53:31 -0400

    new file:   .vscode/launch.json 	modified:   index.html 	new file:   profilepic.webp 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 
+// JavaScript code to toggle the visibility of the contact popup
+
+// Get the contact link and the popup elements
+const contactLink = document.getElementById('contact-link');
+const contactPopup = document.getElementById('contact-popup');
+const closeBtn = document.querySelector('.close');
+
+// Function to show the contact popup
+function showContactPopup() {
+  contactPopup.style.display = 'block';
+}
+
+// Function to hide the contact popup
+function hideContactPopup() {
+  contactPopup.style.display = 'none';
+}
+
+// Add event listeners to the contact link and close button
+contactLink.addEventListener('click', showContactPopup);
+closeBtn.addEventListener('click', hideContactPopup);
+
+// JavaScript code to set the active link
+
 function setActiveLink() {
-    // Get all the navigation links
-    const navLinks = document.querySelectorAll('nav ul li a');
+  // Get all the navigation links
+  const navLinks = document.querySelectorAll('nav ul li a');
 
-    // Add an event listener to each link
-    navLinks.forEach(link => {
-      link.addEventListener('click', (event) => {
-        // Prevent the default link behavior
-        event.preventDefault();
+  // Add an event listener to each link
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      // Prevent the default link behavior
+      event.preventDefault();
 
-        // Remove the 'active' class from all links
-        navLinks.forEach(link => link.classList.remove('active'));
+      // Remove the 'active' class from all links
+      navLinks.forEach(link => link.classList.remove('active'));
 
-        // Add the 'active' class to the clicked link
-        link.classList.add('active');
-      });
+      // Add the 'active' class to the clicked link
+      link.classList.add('active');
     });
-  }
+  });
+}
 
-  // Call the setActiveLink function when the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', setActiveLink);
+// Call the setActiveLink function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', setActiveLink);
